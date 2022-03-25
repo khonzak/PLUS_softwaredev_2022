bd82351711295bbd41a3a7aed7a8312192e12cab
jk4iser <101521592+jk4iser@users.noreply.github.com>
2022-03-25 17:18:29 +0100

    Create Assignment1_Jonas.py

---
+++ Assignment1_Jonas.py
@@ -0,0 +1,6 @@
+# Hello Klara, forgot that the file should contain more than just one thing
+
+# the strength is to be found in serenity
+# I don´t like seafood
+# Lord of the rings bc it is a great movie
+# Schnitzel
