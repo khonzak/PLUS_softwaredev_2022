9af7cfa2d67cd6986bd82516591db5f4bed7ebaa
khonzak <101528297+khonzak@users.noreply.github.com>
2022-03-28 11:18:15 +0200

    Add files via upload

---
+++ hello_recipient.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 11:13:13 2022
+
+@author: klara_000
+"""
+
+import os
+import sys
+
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+def say_hello(recipient):
+    print("Hello, world! Hello {}".formula(recipient))
+    return recipient
